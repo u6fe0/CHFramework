f252425d497defc1251af5bbfbd14234d5fdf75f
chenhao <570768995@qq.com>
2025-10-27 17:13:29 +0800

    【完善】CHButton 可通过菜单快捷添加

--- Client/assets/Scripts/Plugins/UIComponent/CHButton.ts
+++ Client/assets/Scripts/Plugins/UIComponent/CHButton.ts
@@ -1,14 +1,18 @@
-import { _decorator, Button, Component } from 'cc';
+import { _decorator, Button } from 'cc';
 import { Context } from '../../CHFramework/Framework';
 import { AudioService } from '../../Demo_CHFramework/Services/Audio/AudioService';
-const { ccclass, property } = _decorator;
+const { ccclass, property, menu } = _decorator;
 import { Enum } from "cc";
-
+/**
+ * 带点击音效的按钮组件
+ * 继承自cc.Button
+ */
 const ClickAudioType = Enum({
     Normal: "click", // 普通点击音效
 });
 @ccclass('CHButton')
-export class CHButton  extends Button {
+@menu('CHFramework/UI/CHButton')
+export class CHButton extends Button {
     @property({ type: ClickAudioType })
     clickAudioType = ClickAudioType.Normal;
 
